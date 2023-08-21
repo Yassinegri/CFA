e428dcfd9522ac6672e1dfc910057c4cf5159cec
Yassine Grib <YassineGrib@outlook.com>
2023-08-21 14:00:15 +0100

    Add project files.

---
+++ CFA/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+
+
+Partial Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
